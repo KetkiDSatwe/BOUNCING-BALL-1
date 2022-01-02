893856bc40bd5811c77882b6ceea87bd18364693
KetkiDSatwe <91003635+KetkiDSatwe@users.noreply.github.com>
2022-01-02 20:40:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,75 +1,99 @@
-var canvas;
-var block1,block2,block3,block4;
-var ball, edges;
-var music;
+//name spacing
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-function preload(){
-    music = loadSound("music.mp3");
-}
-
+var myengine,myworld;
+var ground, ball;
 
 function setup(){
-    canvas = createCanvas(800,600);
+    var canvas = createCanvas(800,400);
+   
+    myengine = Engine.create();
+    myworld = myengine.world;
 
-    block1 = createSprite(0,580,360,30);
-    block1.shapeColor = "blue";
+    //Code for brown ground
+    var ground_options ={
+        isStatic: true, 
+    }
 
-    block2 = createSprite(295,580,200,30);
-    block2.shapeColor = "orange";
+    ground = Bodies.rectangle(400,390,800,20,ground_options);
+    World.add(myworld,ground);
+    console.log(ground);
 
-    block3 = createSprite(515,580,200,30);
-    block3.shapeColor = "red";
+    // Code for red box
+    var redBox_options ={
+        isStatic: true,
+    }
 
-    block4 = createSprite(740,580,220,30);
-    block4.shapeColor = "green";
+    redBox = Bodies.rectangle(270,320,100,120,redBox_options);
+    World.add(myworld,redBox);
 
-    ball = createSprite(random(20,750),100, 40,40);
-    ball.shapeColor = rgb(255,255,255);
-    ball.velocityX = 4;
-    ball.velocityY = 9;
+    // Code for green box
+    var greenBox_options ={
+        isStatic: true,
+    }
 
+    greenBox = Bodies.rectangle(390,320,100,120,greenBox_options);
+    World.add(myworld,greenBox);
+
+    // Code for yellow box
+    var yellowBox_options ={
+        isStatic: true,
+        angle:-60
+        
+    }
+
+    yellowBox = Bodies.rectangle(590,320,100,20,yellowBox_options);
+    Matter.Body.setAngle(yellowBox,200)
+    World.add(myworld,yellowBox);
+
+    // Code for ball
+    var ball_options ={
+        // isStatic : false,
+        // isStatic: false
+        // isStatic = false,
+        // isStatic : true,
+        restitution: 1.3
+    }
+
+     //ball = Bodies.circle(590,100,20);
+     //World.add(myworld,ball);
+
+    // ball = Bodies.ellipse(590,100,20,
+    //                      ball_options);
+    // World.add(myworld,ball);
+
+     ball = Bodies.circle(590,79,25,
+                         ball_options);
+     World.add(myworld,ball);
+
+    // ball = Bodies(590,100,20);
+    // World.add(myworld,ball);
 }
 
-function draw() {
-    background(rgb(169,169,169));
-    edges=createEdgeSprites();
-    ball.bounceOff(edges);
+function draw(){
+    background("lightgreen");
+    Engine.update(myengine);
+    rectMode(CENTER);
 
-    if( ball.bounceOff(block1)){
-        ball.shapeColor = "blue";
-        music.play();
-    }
-
-    if(block2.isTouching(ball)){
-        ball.shapeColor = "orange";
-        ball.velocityX = 0;
-        ball.velocityY = 0;
-        
-        music.stop;
-        //music=stop();
-        //music.stop();
-        //stop(music);
-    }
-
-    if(block3.isTouching(ball) && ball.bounceOff(block3)){
-        ball.shapeColor = "red";
-    }
-
-    //if(block4(ball) && ball(block4)){
-    //    ball.shapeColor = "green";
-    //}
-
-    //if(block4.isTouching(ball)  ball.bounceOff(block4)){
-    //    ball.shapeColor = "green";
-    //}
-
-    //if(block4.isTouching() && ball.bounceOff()){
-    //    ball.shapeColor = "green";
-    //}
-
-    if(block4.isTouching(ball) && ball.bounceOff(block4)){
-        ball.shapeColor = "green";
-    }
-
-    drawSprites();
+    fill("brown")
+    rect(ground.position.x,ground.position.y,800,20);
+    fill("red")
+    rect(redBox.position.x,redBox.position.y,100,120);
+    fill("green")
+    rect(greenBox.position.x,greenBox.position.y,100,120);
+    
+    fill("yellow")
+    push();
+    translate(yellowBox.position.x,yellowBox.position.y);
+    rotate(-45);
+    rectMode(CENTER);
+    rect(0, 0, 100, 20);
+    pop();
+   
+    fill("red")
+    ellipseMode(RADIUS);
+    ellipse(ball.position.x, ball.position.y, 20, 20);
 }
